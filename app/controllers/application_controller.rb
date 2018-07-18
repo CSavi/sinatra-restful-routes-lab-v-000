65d1976424bee9c557390653ab7a84fdbc05f1ce
Carrie Savinell <carrielane89@gmail.com>
2018-07-18 16:30:12 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -32,7 +32,7 @@ class ApplicationController < Sinatra::Base
   end
 
   patch '/recipes/:id' do
-    binding.pry
+  #  binding.pry
     @recipe = Recipe.find_by_id(params[:id])
     @recipe.update(name: params[:name], ingredients: params[:ingredients], cook_time: params[:cook_time])
 
